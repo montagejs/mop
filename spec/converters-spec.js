75fd393d3a5f85d1ca4493e09445681da2e357b3
Stuart Knightley <stuart@stuartk.com>
2013-06-03 11:18:59 -0700

    Fix converting a document without a doctype to string
    and add test

--- spec/converters-spec.js
+++ spec/converters-spec.js
@@ -3,24 +3,31 @@ var Converters = require("../lib/converters");
 
 describe("Converters", function () {
 
-    it("should convert a string to a buffer", function () {
+    it("converts a string to a buffer", function () {
         var utf8 = "Hello, World!";
         var converter = Converters.from(utf8, "utf8");
         var buffer = converter.to("buffer").content;
         expect(buffer.toString("utf8")).toEqual(utf8);
     });
 
-    it("should convert a buffer to a string", function () {
+    it("converts a buffer to a string", function () {
         var buffer = new Buffer("Hello, World!", "utf8");
         var string = Converters.from(buffer, "buffer").to("utf8").content;
         expect(string).toBe("Hello, World!");
     });
 
-    it("should convert a buffer to a document and back to a string", function () {
+    it("converts a buffer to a document and back to a string", function () {
         var buffer = new Buffer("<!doctype html><h1>Hello</h1>", "utf8");
         var document = Converters.from(buffer, "buffer").to("document").content;
         var string = Converters.from(document, "document").to("utf8").content;
         expect(string.trim()).toBe("<!doctype html><h1>Hello</h1>");
+    });
+
+    it("round-trips an incomplete document correctly", function () {
+        var original = "<h1>Hello</h1>";
+        var document = Converters.from(original, "utf8").to("document").content;
+        var string = Converters.from(document, "document").to("utf8").content;
+        expect(string).toBe(original);
     });
 
 });
