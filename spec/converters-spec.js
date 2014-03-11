866f57009cca8d03138676c818636d158bf89653
Stuart Knightley <stuart@stuartk.com>
2014-03-11 13:56:38 -0700

    Replace JSDom with minidom

--- spec/converters-spec.js
+++ spec/converters-spec.js
@@ -17,17 +17,10 @@ describe("Converters", function () {
     });
 
     it("converts a buffer to a document and back to a string", function () {
-        var buffer = new Buffer("<!doctype html><h1>Hello</h1>", "utf8");
+        var buffer = new Buffer("<!DOCTYPE html><html><head></head><body><h1>Hello</h1></body></html>", "utf8");
         var document = Converters.from(buffer, "buffer").to("document").content;
         var string = Converters.from(document, "document").to("utf8").content;
-        expect(string.trim()).toBe("<!doctype html><h1>Hello</h1>");
-    });
-
-    it("round-trips an incomplete document correctly", function () {
-        var original = "<h1>Hello</h1>";
-        var document = Converters.from(original, "utf8").to("document").content;
-        var string = Converters.from(document, "document").to("utf8").content;
-        expect(string).toBe(original);
+        expect(string.trim()).toBe("<!DOCTYPE html><html><head></head><body><h1>Hello</h1></body></html>");
     });
 
 });
